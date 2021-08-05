713b936765b9bbee649aac73f92915c0e5d71a02
RillA-16 <68328998+RillA-16@users.noreply.github.com>
2021-08-05 15:17:03 +0900

    인프런 스프링 핵심원리_spring Configuration과 바이트코드

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -28,5 +28,17 @@ public class ConfigurationSingletonTest {
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+        System.out.println("sdfsdfsd");
+    }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // AppConfig가 출력되는게 아니라 다른 이름으로 출력되는 이유는 spring내에서 조작한 것을 등록하기 때문에 다르게 보인다.
+        // Configuration에 등록이되면 조작되어진다. -> 결론 싱글톤도 깨질뿐더러 bean 등록제어도 안된다.
+        System.out.println("bean = " + bean.getClass());
     }
 }

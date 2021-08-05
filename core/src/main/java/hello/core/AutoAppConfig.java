6939580eb5a2ec32f7a52243d9a423672cf3280e
RillA-16 <68328998+RillA-16@users.noreply.github.com>
2021-08-05 17:13:24 +0900

    인프런 스프링 핵심원리_spring 컴포넌트 탐색 위치

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(
+        // default값은 hello.core패키지를 다 뒤진다.
+        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 
